946cb1b6839f21218e5d65272be7f0e670ecaf55
rlaeodus <gwg03185@gmail.com>
2024-01-26 17:28:03 +0900

    ex4

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 
 // TODO: FilterButtons 컴포넌트를 작성하세요. 필터링 옵션을 선택하는 버튼들을 포함해야 합니다.
-function FilterButtons(/* 필요한 props를 여기에 전달하세요 */ {}) {
-  const filterByAge = (minAge) => {};
-  const filterByGrade = (grade) => {};
-  const resetFilter = () => {};
+function FilterButtons(
+  /* 필요한 props를 여기에 전달하세요 */ {
+    filteredStudents,
+    setFilteredStudents,
+  }
+) {
+  const filterByAge = (minAge) => {
+    const filterAge = filteredStudents.filter((x) => x.age >= minAge);
+    setFilteredStudents(filterAge);
+  };
+  const filterByGrade = (grade) => {
+    const filterGrade = filteredStudents.filter((x) => x.grade === grade);
+    setFilteredStudents(filterGrade);
+  };
+  const resetFilter = () => {
+    setFilteredStudents(filteredStudents);
+  };
 
   return (
     <div>
       {/* 여기에 필터링 버튼들을 완성하세요. */}
-      <button>18세 이상</button>
-      <button>A등급</button>
-      <button>필터 초기화</button>
+      <button onClick={() => filterByAge()}>18세 이상</button>
+      <button onClick={() => filterByGrade()}>A등급</button>
+      <button onClick={() => resetFilter()}>필터 초기화</button>
     </div>
   );
 }
